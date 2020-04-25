f56943e6b910a34e094966f0f848af518d456c76
Kostya Vinogradov <kostya.06.05.1995@gmaul.com>
2020-04-25 23:25:38 +0300

    Выполнили коректировку расчёта ускорения в солнечной системе

--- solar_model.py
+++ solar_model.py
@@ -18,19 +18,37 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        vector_x = body.x - obj.x
-        vector_y = body.y - obj.y
+        
+        # первый вариант
+        vector_x = obj.x - body.x
+        vector_y = obj.y - body.y
+
         r = (vector_x**2 + vector_y**2)**0.5
         ed_vector_x = vector_x / r
         ed_vector_y = vector_y / r
 
-        power_gravity = gravitational_constant * (body.m+obj.m) / (r**2)
+        power_gravity = gravitational_constant * (body.m*obj.m) / (r**2)
 
         ed_vector_x *= power_gravity
         ed_vector_y *= power_gravity
 
-        body.Fx += ed_vector_x * power_gravity
-        body.Fy += ed_vector_y * power_gravity
+        body.Fx += ed_vector_x / body.m
+        body.Fy += ed_vector_y / body.m
+
+        # Второй вариант
+        #vector_x = obj.x - body.x
+        #vector_y = obj.y -body.y
+        #r = (vector_x**2 + vector_y**2)**0.5
+        #body.Fx += gravitational_constant * obj.m * vector_x / r**3
+        #body.Fy += gravitational_constant * obj.m * vector_y / r**3
+
+        # Третий вариант
+        # tmp_len = vector_x**2 + vector_y**2
+        # f = (gravitational_constant*obj.m) / tmp_len * (tmp_len**0.5)
+        # body.Fx += vector_x * f
+        # body.Fy += vector_y * f
+
+        t = 0
 
 
 def move_space_object(body, dt):
@@ -39,14 +57,18 @@ def move_space_object(body, dt):
     Параметры:
 
     **body** — тело, которое нужно переместить.
+    **dt** — шаг по времени
     """
     # len_v = (body.Vx**2+body.Vy**2)**0.5
     # len_f = (body.Fx**2+body.Fy**2)**0.5
     # fx = body.Fx / len_f
     # fy = body.Fy / len_f
 
-    body.x += body.Vx + body.Fx
-    body.y += body.Vy + body.Fy
+    body.Vx += body.Fx * dt
+    body.Vy += body.Fy * dt
+
+    body.x += body.Vx * dt
+    body.y += body.Vy * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
